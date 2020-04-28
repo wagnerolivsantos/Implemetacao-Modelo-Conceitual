1b1a0faa5c7bcb44d9744535c62f6b1e64bebfee
Wagner Oliveira <wagnerolivsantos@outlook.com>
2020-04-28 05:24:26 -0300

    Projeto Criado

---
+++ src/test/java/com/mskadven123/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mskadven123.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
